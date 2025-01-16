0ad44c062a9072c09fd4b884a54c969171374de5
rreenactt <reenactPi@gmail.com>
2025-01-16 20:44:27 +0900

    ADD PopManager

---
+++ Assets/01_Scripts/Base/PopBase.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopBase : MonoBehaviour
+{
+    public virtual void Open()
+    {
+
+    }
+    public virtual void Close()
+    {
+
+    }
+}
