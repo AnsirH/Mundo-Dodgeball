d383992485825b4cac2c5aa67b7817f2f5889441
rreenactt <reenactPi@gmail.com>
2025-01-16 20:51:56 +0900

    Add PopUPOpen, Close Fun

--- Assets/01_Scripts/Manager/PopManager.cs
+++ Assets/01_Scripts/Manager/PopManager.cs
@@ -5,7 +5,29 @@ using UnityEngine;
 public class PopManager : ManagerBase<PopManager>
 {
     private List<PopBase> openPops = new List<PopBase>();
-
+    
+    public void OpenPop(PopBase pop)
+    {
+        if(!openPops.Contains(pop))
+        {
+            openPops.Add(pop);
+        }
+    }
+    public void ClosePop(PopBase pop, bool isAllClose)
+    {
+        if (openPops.Contains(pop))
+        {
+            openPops.Remove(pop);
+        }
+        if(isAllClose)
+        {
+            foreach(PopBase p in openPops)
+            {
+                p.gameObject.SetActive(false);
+            }
+            openPops.Clear();
+        }
+    }
     void Start()
     {
         
