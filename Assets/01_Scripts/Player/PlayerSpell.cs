982f070db09717b71f6aff34e27ee7333bd1e448
ansirH <ansir0211@gmail.com>
2025-02-14 02:20:06 +0900

    fix PlayerSpell.cs

--- Assets/01_Scripts/Player/PlayerSpell.cs
+++ Assets/01_Scripts/Player/PlayerSpell.cs
@@ -1,38 +1,30 @@
+using Photon.Pun.Demo.Cockpit;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerSpell : MonoBehaviour
 {
+    private void Awake()
+    {
+        spellD = new Flash(this);
+    }
+
+    private void Update()
+    {
+        if (!spellD.CanUsable)
+        {
+            spellD.CoolDown(Time.deltaTime);
+        }
+    }
+
     public void OnSpellD()
     {
         if (canUseSpellD)
         {
-            RaycastHit hit;
-            if (Physics.Raycast(CameraManager.Instance.firstPlayerCamera.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, LayerMask.GetMask("Ground")))
-            {
-                Vector3 mousePoint = hit.point;
-                mousePoint.y = transform.position.y;
-                Flash(mousePoint);
-
-                canUseSpellD = false;
-                Invoke("SetSpellable", 5.0f);
-            }
+            spellD.Execute();
         }
-    }
-
-    private void Flash(Vector3 targetPoint)
-    {
-        Vector3 targetVector = targetPoint - transform.position;
-        if (targetVector.magnitude > flashDistance)
-        {
-            transform.position += targetVector.normalized * flashDistance;
-        }
-        else
-        {
-            transform.position += targetVector;
-        }
-        transform.rotation = Quaternion.LookRotation(targetVector);
     }
 
     private void SetSpellable()
@@ -40,22 +32,65 @@ public class PlayerSpell : MonoBehaviour
         canUseSpellD = true;
     }
 
+    private Spell spellD;
+
     public float flashDistance = 1.5f;
     public bool canUseSpellD = true;
 }
 
 public class Spell
 {
+    public PlayerSpell owner;
+    public Spell(PlayerSpell owner)
+    {
+        this.owner = owner;
+    }
+
     public virtual void Execute()
     {
-
+        if (!CanUsable) { return; }
     }
+
+    public void CoolDown(float decreaseValue)
+    {
+        if (currentCoolTime > 0.0f)
+        {
+            currentCoolTime -= decreaseValue;
+        }
+        else
+        {
+            currentCoolTime = 0.0f;
+        }
+    }
+
+    public float currentCoolTime = 0.0f;
+    public float maxCoolTime = 5.0f;
+
+    public bool CanUsable { get { return currentCoolTime <= 0.0f; } }
 }
 
 public class Flash : Spell
 {
+    public Flash(PlayerSpell owner) : base(owner)
+    {
+    }
+
     public override void Execute()
     {
+        base.Execute();
+        Vector3 targetVector = PlayerController.GetMousePosition(CameraManager.Instance.firstPlayerCamera, owner.transform) - owner.transform.position;
+        if (targetVector.magnitude > distance)
+        {
+            owner.transform.position += targetVector.normalized * distance;
+        }
+        else
+        {
+            owner.transform.position += targetVector;
+        }
+        owner.transform.rotation = Quaternion.LookRotation(targetVector);
 
+        currentCoolTime = maxCoolTime;
     }
+
+    float distance = 3.0f;
 }
