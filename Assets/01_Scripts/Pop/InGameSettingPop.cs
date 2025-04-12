d8c30eb1d53678584b071aacc0142bb083c9c015
rreenactt <91408385+rreenactt@users.noreply.github.com>
2025-04-12 17:31:28 +0900

    upload ingamesetting pop

---
+++ Assets/01_Scripts/Pop/InGameSettingPop.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class InGameSettingPop : PopBase
+{
+    public override void Open()
+    {
+        base.Open();
+    }
+    public override void Close()
+    {
+        base.Close();
+    }
+    public void GiveUpClick()
+    {
+
+    }
+}
